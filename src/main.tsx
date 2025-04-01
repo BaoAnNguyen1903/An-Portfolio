a5ab5dacb0257f7cfa6913d80b6ff20beb46f898
An Nguyen <anndb1903@gmail.com>
2025-04-01 14:44:39 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Layout from './layout.tsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";  
+import HomePage from './pages/home.tsx';
+import ProjectPage from './pages/project.tsx';
+import AboutPage from './pages/about.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/projects",
+        element: <ProjectPage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Layout />
+    {/* <AppContextProvider> */}
+      <RouterProvider router={router} />
+    {/* </AppContextProvider> */}
   </StrictMode>,
 )
