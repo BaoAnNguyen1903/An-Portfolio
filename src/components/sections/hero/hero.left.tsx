90d4b82c7baaccda809494e313c4a9136b3aee74
An Nguyen <anndb1903@gmail.com>
2026-05-12 11:41:05 +0700

    new CV

--- src/components/sections/hero/hero.left.tsx
+++ src/components/sections/hero/hero.left.tsx
@@ -21,7 +21,7 @@ const HeroLeft = (props: IProps) => {
 
   const handleDownload = () => {
     openNewtab(
-      "https://drive.google.com/file/d/1OAWfiJ0LkBv3917hlrWVtWhyrLjnAnp5/view?usp=sharing"
+      "https://drive.google.com/drive/folders/1sNYfKEsZCpVyNC8NQ1Juzt8EUSS7B2-F"
     );
   };
   return (
